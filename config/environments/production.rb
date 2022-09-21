e1013088ae9e02178b16a2f36783f78d23d843a2
Lucas dos Santos <lucas.ffr2014@gmail.com>
2022-09-21 03:10:27 -0300

    Fix dashboard and production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.public_file_server.enabled = true
   config.assets.compile = true
